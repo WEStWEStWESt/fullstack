9ea9f5c11c58f329bf3ac47f37ed18c6ee62d13b
WESt <AlexWESt101087@mail.ru>
2024-12-18 22:53:24 +0300

    Create the backend project for the WEBSITE-PUBLIC.

---
+++ src/main/java/com/west/fullstack/JwtCore.java
@@ -0,0 +1,12 @@
+package com.west.fullstack;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtCore {
+   /* @Autowired("${fullstack.app.secret}")
+    private String secret;
+    @Autowired("${fullstack.app.lifetime}")
+    private int lifetime;*/
+}
