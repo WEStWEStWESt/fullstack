27574cb5dd7e14fa9494ccdd0f20103e2515c58b
WESt <AlexWESt101087@mail.ru>
2024-12-19 22:54:24 +0300

    Сonnecting a user to the database...

--- src/main/java/com/west/fullstack/JwtCore.java
+++ src/main/java/com/west/fullstack/JwtCore.java
@@ -1,12 +1,25 @@
 package com.west.fullstack;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+
+import java.util.Date;
 
 @Component
 public class JwtCore {
-   /* @Autowired("${fullstack.app.secret}")
+    @Value("${fullstack.app.secret}")
     private String secret;
-    @Autowired("${fullstack.app.lifetime}")
-    private int lifetime;*/
+    @Value("${fullstack.app.lifetime}")
+    private int lifetime;
+
+    //  difference  !!!!!! new Date(System.currentTimeMillis() + lifetime))
+    public String generateToken(Authentication authentication) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + lifetime))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
 }
---
+++ src/main/java/com/west/fullstack/User.java
@@ -0,0 +1,21 @@
+package com.west.fullstack;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+    @Column
+    private String email;
+    @Column
+    private String password;
+}
